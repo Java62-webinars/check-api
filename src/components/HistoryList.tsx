87f956b4e8292fe1abca1d45dfb4a86425102c6d
matveyromodanovskiy <startpolus@gmail.com>
2025-12-17 18:38:18 +0300

    add Histor

--- src/components/HistoryList.tsx
+++ src/components/HistoryList.tsx
@@ -1,12 +1,21 @@
+import { useSelector } from "react-redux";
+import type { RootState } from "../store/store";
 
+export function HistoryList() {
+    const history = useSelector((state: RootState) => state.history);
 
-const HistoryList = () => {
-    //TODO develop history with localStorage
+    if (history.length === 0) return <div>История запросов пуста.</div>;
+
     return (
         <div>
-
+            <h2>История запросов</h2>
+            <ul>
+                {history.map((ip, idx) => (
+                    <li key={`${ip}-${idx}`}>{ip}</li>
+                ))}
+            </ul>
         </div>
     );
-};
+}
 
 export default HistoryList;
